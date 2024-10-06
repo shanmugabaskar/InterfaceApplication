ffce732a3cf9abba4a63dd9b5020f758730e8b4d
Admin <Admin@JIVIN-DIYA-PC>
2024-10-06 23:40:00 +0530

    Add project files.

---
+++ InterfaceApplication/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using InterfaceApplication;
+
+Transaction t1 = new Transaction("001", "8/10/2012", 78900.00);
+Transaction t2 = new Transaction("002", "9/10/2012", 451900.00);
+
+t1.showTransaction();
+t2.showTransaction();
+Console.ReadKey();
